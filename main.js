6fb51790d45153651043fddac66170e131f5cc1c
guicondee <guicondee@outlook.com>
2022-06-13 10:15:35 -0300

    function activateMenuAtCurrentSection created

--- main.js
+++ main.js
@@ -4,6 +4,48 @@
 function onScroll() {
   showNavOnScroll()
   showbackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  //  verificar se a seçao passou da linha
+  // quais dados vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo?
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou a linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites de seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
